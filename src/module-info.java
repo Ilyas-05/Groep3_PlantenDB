d1ca7e293764df3855b89c217dc2cfcc7e409222
Ilyas <ilyas.isaev@student.vives.be>
2020-02-19 14:03:35 +0100

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module Groep3.PlantenDB {
+    requires java.desktop;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+
+    opens sample;
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Controller {
+    public Label lblResultaat;
+
+    public void clicked_Vorige(MouseEvent mouseEvent) {
+    }
+
+    public void clicked_Volgende(MouseEvent mouseEvent) {
+    }
+}
